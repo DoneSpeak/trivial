dd41828cdf0e50014001c8d2d529fbc6a4e31a3f
DoneSpeak <1261831729@qq.com>
2016-05-11 13:30:01 +0800

    可以下载url指定文件，同时可以用android手机的默认程序打开

---
+++ app/src/main/java/donespeak/com/netsourcedownload/RunReturnType.java
@@ -0,0 +1,13 @@
+package donespeak.com.netsourcedownload;
+
+/**
+ * Created by glorior on 2016/5/10.
+ */
+public class RunReturnType {
+    public static final int SDNotExit = 0;
+    public static final int DOWNLOADFAIL = 1;
+    public static final int STARTDOWNLOAD = 2;
+    public static final int FINISHDOWNLOAD = 3;
+    public static final int STOPDOWNLOAD = 4;
+    public static final int FAILTOGETNAME = 5;
+}
